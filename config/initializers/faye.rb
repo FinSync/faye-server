c315892fd7ad8ad74fc7a919a193303b305ee746
Brian David Wetzel <briandavidwetzel@gmail.com>
2017-12-18 11:24:33 -0500

    Upgrade

--- config/initializers/faye.rb
+++ config/initializers/faye.rb
@@ -9,7 +9,7 @@ FayeServer.config.tap do |config|
   config[:timeout]= Integer(ENV['FAYE_TIMEOUT'] || 25)
   config[:ping]=    Integer(ENV['FAYE_KEEPALIVE'] || 45)
 
-  redis_url = ENV['REDISTOGO_URL'] || ENV['FAYE_REDIS_URL']
+  redis_url = ENV['REDIS_URL'] || ENV['REDISTOGO_URL'] || ENV['FAYE_REDIS_URL']
   if redis_url
     config[:engine] = { type: Faye::Redis, uri: redis_url }
   end
